812f36f78a6bb85647f7e996f7bc8e31b3409cc7
awan-bilal <awan-bilal@gmail.com>
2024-08-10 22:38:46 +0500

    first commit

--- src/config.js
+++ src/config.js
@@ -61,7 +61,9 @@ export const defaultLang = allLangs[0]; // English
 // DEFAULT ROOT PATH
 export const DEFAULT_PATH = PATH_DASHBOARD.general.app; // as '/app'
 export const BASE_URL = "http://13.48.27.232:8000/api/";
-export const SOCKET_BASE_URL = "ws://13.48.27.232:5000";
+// export const BASE_URL = "http://127.0.0.1:8000/api/";
+
+export const SOCKET_BASE_URL = "http://127.0.0.1:8000";
 
 
 // http://13.48.27.232:5000
--- src/services/userservice.js
+++ src/services/userservice.js
@@ -4,16 +4,16 @@ const USER_KEY = "chatuser";
 const USER_TOKEN = "token";
 
 export const getUserToken = () => {
-  const user = localStorage.getItem(USER_KEY);
-  if(user) {
-      const userData = JSON.parse(user);
-      if(userData.access_token) {
-        return userData.access_token;
-      }else {
-        return null;
-      }
-  }
-  return null;
+  // const user = localStorage.getItem(USER_KEY);
+  // if(user) {
+  //     const userData = JSON.parse(user);
+  //     if(userData.access_token) {
+  //       return userData.access_token;
+  //     }else {
+  //       return null;
+  //     }
+  // }
+  return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzUzODIzMDcxLCJpYXQiOjE3MjE3NTY2MDYsImp0aSI6ImM2YmZkODY1NzBlNTQyMjM5NmI2NjA2OWVhMDczMTJmIiwidXNlcl9pZCI6NX0.yb9vICSMKkeUVXpMzB4H3V6DPJY5WaHUBVohoTj_sv0';
 };
 export const getUserInfo = () => {
   const user = localStorage.getItem(USER_KEY);
